adff12183d69cd5dde132a86daa6d5258964578e
zannatulferdousianika <zfanika79@gmail.com>
2025-04-22 22:49:02 +0600

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,43 @@
+print("Simple Calculator")
+
+print("Addition")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("result: ",a+b)
+
+print("Substraction")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("result: ",a-b)
+
+print("Multiplication")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("result: ",a*b)
+
+
+print("Division")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("result: ",a/b)
+
+print("Exponentiation")
+a = int(input("Enter the first number: "))
+print("square: ",a**2)
+print("Cube: ", a**3)
+
+print("Reminder")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("result: ",a%b)
+
+print("Floor Division")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("result: ",a//b)
+
+
+
+
+
+
