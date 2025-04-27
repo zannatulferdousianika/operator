bf3685f4ac02d5de119b1a4a1abb21162fc829e3
zannatulferdousianika <zfanika79@gmail.com>
2025-04-28 01:21:08 +0600

    Add files via upload

--- stringoperation.py
+++ stringoperation.py
@@ -5,3 +5,98 @@ print(a)
 b= "hello"
 b = b.upper()
 print(b)
+
+g = "hello"
+g = g.lower()
+print(g)
+
+
+txt = "Hey there, My name is Anika. What about yours ?"
+c = txt.capitalize()
+print(c)
+
+d = "WWoo..Hey nIce to mEEt UuU "
+d = d.casefold()
+print(d)
+
+e = "apple"
+e = e.center(20)
+print(e)
+
+f = "258025578555852"
+f = f.count("5")
+print(f)
+
+g = "ENCODEEE"
+g = g.encode()
+print(g)
+
+h = "why do u love code ?"
+h = h.endswith(".")
+print(h)
+
+i = "H\te\tl\tl\to"
+i = i.expandtabs(2)
+print(i)
+
+j="helooo"
+j.find(e)
+print(j)
+
+l = "its {price:.2f}"
+print(l.format(price = 49))
+
+k = "bullshit"
+k = k.index("s")
+print(k)
+
+m = "hey38"
+m = m.isalpha()
+print(m)
+
+n = "companyX"
+n = n.isalnum()
+print(n)
+
+o = "@@#$%*^"
+o = o.isascii()
+print(o)
+
+p = "50.11514648"
+p = p.isdecimal()
+print(p)
+
+q = "23156688"
+q = q.isdigit()
+print(q)
+
+r = "good1256"
+r = r.isidentifier()
+print(r)
+
+s="25897845"
+s = s.isnumeric()
+print(s)
+
+v = "hiiii there555"
+v = v.isprintable()
+print(v)
+
+
+w = "It Doesn't looks like a title"
+w = w.istitle()
+print(w)
+
+o2 = "hiiii there555"
+o2 = o2.isspace()
+print(o2)
+
+x = "John", "Peter", "Vicky"
+x = "wulluu".join(x)
+print(x)
+
+T = "hiiii there555"
+T = T.partition("there")
+print(T)
+
+
