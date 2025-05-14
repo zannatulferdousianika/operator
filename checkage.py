7c0fa382853298df39cdf04de8bded2575429b7d
zannatulferdousianika <zfanika79@gmail.com>
2025-05-15 00:59:19 +0600

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,9 @@
+a = float(input("Enter your age number: "))
+if 0 < a < 16:
+    print("It's a babyyy!!!")
+elif 17 < a < 30:
+    print("You are a young adult!!")  
+elif 31 < a < 45:
+    print("You are Middle aged ") 
+else:
+    print("You are old aged!")        
